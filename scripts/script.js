87a983c0bbde02068f04bcad9351a8247203d9fc
Camilo Andrés Gutiérrez Barriga <camiloguba@unisabana.edu.co>
2025-08-27 21:37:05 -0500

    HTML Y CSS Terminado

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-console.log("JavaScript working!");
+// Variable to tasks array
+let tasks = [];
+let taskIdCounter = 0;
+
+// Dom Interaction Elements
+const taskInput =document.getElementById('task-input');
+const addButton= document.getElementById('add-task-button');
+const tasksList = document.getElementById('task-list');
